cc7c6098011c8bb04daae0b300e0f34ce5eba231
mrustl <michael.rustler@kompetenz-wasser.de>
2019-01-16 23:34:32 +0100

    Install kwb-r pkgs before running vignette and
    skip installing pkgs on Travis within vignette using withr:: functions

---
+++ R/travis_pkg_install_script.R
@@ -0,0 +1,15 @@
+#' Travis Package Install Script
+#' @description Needs to be Run before rendering vignettes
+#' @param pkgs datadframe with R packages as retrieved by
+#' get_github_packages()
+#' @return installs kwb-r packages
+#' @export
+travis_pkg_install_script <- function(pkgs = pkgmeta::get_github_packages()) {
+
+for(pkg in pkgs$name) {
+
+remotes::install_github(repo = sprintf("kwb-r/%s", pkg),
+                                   dependencies = TRUE,
+                                   upgrade = "always")
+}
+}

7c070825cc086d892dcf1f1b794c0eb7fd755577
mrustl <michael.rustler@kompetenz-wasser.de>
2019-01-17 17:59:20 +0100

    Fix :bug: in write_pkg_codemeta()

--- R/write_pkg_codemeta.R
+++ R/write_pkg_codemeta.R
@@ -2,19 +2,18 @@
 #'
 #' @param codemeta codemeta object (as retrieved by create_codemeta()),
 #' default: create_pkg_codemeta())
-#' @param file path where to save codemeta.json
+#' @param file path where to save codemeta.json (default: file.path(getwd(),
+#' "codemetar.json"))
 #' @param dbg should debug messages be printed? (default: TRUE)
 #' @return writes "codemeta.json"
 #' @importFrom jsonlite write_json
 #' @importFrom kwb.utils catAndRun
-#' @importFrom fs path_real
 #' @export
 write_pkg_codemeta <- function(codemeta = create_pkg_codemeta(),
-                               file = "./codemetar.json",
+                               file = file.path(getwd(), "codemetar.json"),
                                dbg = TRUE) {
 
-  kwb.utils::catAndRun(sprintf("Writting codemeta to '%s'",
-                               fs::path_real(file)),
+  kwb.utils::catAndRun(sprintf("Writting codemeta to '%s'", file),
                        expr = {
                          jsonlite::write_json(codemeta, file,
                                               useBytes = TRUE,

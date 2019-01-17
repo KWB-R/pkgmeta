854e67e0f9b1041b67f48e67d0b948742555bd71
mrustl <michael.rustler@kompetenz-wasser.de>
2019-01-17 16:30:34 +0100

    Add "dbg" argument and execute codemetar generation
    on Travis

--- R/create_pkg_codemeta.R
+++ R/create_pkg_codemeta.R
@@ -3,14 +3,19 @@
 #'
 #' @param pkgs data frame with R packages (default: get_github_packages())
 #' @param libpath libpath  (default: "/home/travis/R/Library")
+#' @param dbg should debug messages be printed? (default: TRUE)
 #' @return codemetar object for R packages
 #' @importFrom codemetar create_codemeta
 #' @importFrom glue glue
 #' @export
 create_pkg_codemeta <- function(pkgs = get_github_packages(),
-                            libpath = "/home/travis/R/Library") {
+                            libpath = "/home/travis/R/Library",
+                            dbg = TRUE) {
 
-withr::with_libpaths(new = libpath,
+
+  kwb.utils::catAndRun("Creating codemeta object",
+                       expr = {
+  withr::with_libpaths(new = libpath,
                      code = {
                        lapply(pkgs$name,
   function(x) {
@@ -19,7 +24,8 @@ withr::with_libpaths(new = libpath,
   codemetar::create_codemeta(pkg = x)}
   else {
   message(sprintf("Package '%s' is not installed in
-  %s", x, libpath))}})})
+  %s", x, libpath))}})})},
+  dbg = dbg)
 
 
 }
--- R/write_pkg_codemeta.R
+++ R/write_pkg_codemeta.R
@@ -3,16 +3,24 @@
 #' @param codemeta codemeta object (as retrieved by create_codemeta()),
 #' default: create_pkg_codemeta())
 #' @param file path where to save codemeta.json
+#' @param dbg should debug messages be printed? (default: TRUE)
 #' @return writes "codemeta.json"
 #' @importFrom jsonlite write_json
+#' @importFrom kwb.utils catAndRun
+#' @importFrom fs path_real
 #' @export
 write_pkg_codemeta <- function(codemeta = create_pkg_codemeta(),
-                               file = "codemetar.json") {
+                               file = "./codemetar.json",
+                               dbg = TRUE) {
 
-  jsonlite::write_json(codemeta, file,
-                       useBytes = TRUE,
-                       pretty = TRUE,
-                       auto_unbox = TRUE)
+  kwb.utils::catAndRun(sprintf("Writting codemeta to '%s'",
+                               fs::path_real(file)),
+                       expr = {
+                         jsonlite::write_json(codemeta, file,
+                                              useBytes = TRUE,
+                                              pretty = TRUE,
+                                              auto_unbox = TRUE)},
+                       dbg = dbg)
 
 
 }

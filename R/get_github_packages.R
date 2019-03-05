502fe94ffe7d65da1abc7bc3cb6f0cab4a65579b
mrustl <michael.rustler@kompetenz-wasser.de>
2019-03-05 11:42:55 +0100

    add new parameter "non_r_packages"
    to filter out repos that are not R packages (in order to enable better reuse of this function)

--- R/get_github_packages.R
+++ R/get_github_packages.R
@@ -3,6 +3,8 @@
 #' @param ignore_pkgs vector of pkg names that should be ignored to prevent
 #' problems with codemeta generation (e.g. "kwb.read" fails due to missing
 #' metadata on the license!)
+#' @param non_r_packages vector with repos that are not R packages (default:
+#' kwb.pkgstatus::get_non_r_packages())
 #' @param github_token optionally a Github token in order to access private
 #' repositories (default: getOption("github_token"))
 #' @return data frame with R packages on Github
@@ -16,10 +18,11 @@
 #'
 get_github_packages <- function(group = "KWB-R",
                                 ignore_pkgs = NULL,
+                                non_r_packages = kwb.pkgstatus::get_non_r_packages(),
                                 github_token = getOption("github_token")) {
   repos <- kwb.pkgstatus::get_github_repos(group, github_token)
 
-  pkgs <- repos[!repos$name %in% kwb.pkgstatus::get_non_r_packages(), ]
+  pkgs <- repos[!repos$name %in% non_r_packages, ]
 
   if (!is.null(ignore_pkgs)) {
     ignore_condition <- pkgs$name %in% ignore_pkgs

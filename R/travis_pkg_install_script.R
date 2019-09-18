1bc22f89dc49fc39b2db2e12929a7dddc1839e7e
mrustl <michael.rustler@kompetenz-wasser.de>
2019-09-18 16:14:32 +0200

    Skip package if installation fails

--- R/travis_pkg_install_script.R
+++ R/travis_pkg_install_script.R
@@ -1,15 +1,18 @@
 #' Travis Package Install Script
 #' @description Needs to be Run before rendering vignettes
-#' @param pkgs datadframe with R packages as retrieved by
+#' @param pkgs dataframe with R packages as retrieved by
 #' get_github_packages()
 #' @return installs kwb-r packages
 #' @export
 travis_pkg_install_script <- function(pkgs = pkgmeta::get_github_packages()) {
-  for (pkg in pkgs$name) {
-    remotes::install_github(
-      repo = sprintf("kwb-r/%s", pkg),
-      dependencies = TRUE,
-      upgrade = "always"
-    )
-  }
+  sapply(
+    pkgs$full_name,
+    FUN = function(pkg) {
+      try(remotes::install_github(
+        repo = pkg,
+        dependencies = TRUE,
+        upgrade = "always"
+      ))
+    }
+  )
 }

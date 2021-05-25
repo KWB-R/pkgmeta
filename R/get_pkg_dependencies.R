133366e8e37da269a132a57822ac23f787523b4b
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-05-25 17:36:13 +0200

    Add function get_pkg_dependencies()
    and make it more flexible

--- R/get_pkg_dependencies.R
+++ R/get_pkg_dependencies.R
@@ -1,21 +1,22 @@
-#' Get recursive package dependencies
+#' Get package dependencies
 #'
 #' @param pkgs character vector with package names
 #' @param library_path character vector with path(s) to R library (default: .libPaths())
 #' @param dbg logical if debug should be shown (default: TRUE)
 #' @param ... additional arguments passed to \link[packrat]{getPackageDependencies}
-#' @return list with recursive package dependencies
+#' @return list with package dependencies
 #' @importFrom stats setNames
 #' @export
 #' @examples
 #' \dontrun{
 #' pkgs <- pkgmeta::get_github_packages()
-#' get_recursive_pkg_dependencies(pkgs$name)
+#' get_pkg_dependencies(pkgs$name)
 #' }
 #'
-get_recursive_pkg_dependencies <- function(pkgs, library_path = .libPaths(),
-                                           dbg = TRUE,
-                                           ...) {
+get_pkg_dependencies <- function(pkgs,
+                                 library_path = .libPaths(),
+                                 dbg = TRUE,
+                                 ...) {
 
 
   pkgs_installed <- pkgs[pkgs %in% rownames(installed.packages(lib.loc = library_path))]
@@ -23,9 +24,11 @@ get_recursive_pkg_dependencies <- function(pkgs, library_path = .libPaths(),
   stats::setNames(lapply(pkgs_installed, function(pkg) {
     kwb.utils::catAndRun(sprintf("Getting recursive dependencies for '%s'", pkg),
                          expr = {
-                           packrat:::recursivePackageDependencies(pkg,
-                                                                  lib.loc = library_path,
-                                                                  ...)},
+                           packrat:::getPackageDependencies(pkg,
+                                                            lib.loc = library_path,
+                                                            ...
+                                                            )
+                           },
                          dbg = dbg)}),
     nm = pkgs_installed)
 

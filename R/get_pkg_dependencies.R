d96b7e5efdf1ffc69697d3372e444267b27360ae
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-05-02 04:04:41 +0200

    Put dependency functions into one file

--- R/get_pkg_dependencies.R
+++ R/get_pkg_dependencies.R
@@ -1,3 +1,5 @@
+# get_pkg_dependencies ---------------------------------------------------------
+
 #' Get package dependencies
 #'
 #' @param pkgs character vector with package names
@@ -23,6 +25,39 @@ get_pkg_dependencies <- function(
   get_pkg_dependencies_impl(
     pkgs,
     recursive = FALSE,
+    ...,
+    library_path = library_path,
+    dbg = dbg
+  )
+}
+
+# get_recursive_pkg_dependencies -----------------------------------------------
+
+#' Get recursive package dependencies
+#'
+#' @param pkgs character vector with package names
+#' @param library_path character vector with path(s) to R library (default: .libPaths())
+#' @param dbg logical if debug should be shown (default: TRUE)
+#' @param ... additional arguments passed to packrat:::getPackageDependencies()
+#' @return list with recursive package dependencies
+#' @importFrom stats setNames
+#' @export
+#' @examples
+#' \dontrun{
+#' pkgs <- pkgmeta::get_github_packages()
+#' get_recursive_pkg_dependencies(pkgs$name)
+#' }
+#'
+get_recursive_pkg_dependencies <- function(
+    pkgs,
+    library_path = .libPaths(),
+    dbg = TRUE,
+    ...
+)
+{
+  get_pkg_dependencies_impl(
+    pkgs,
+    recursive = TRUE,
     ...,
     library_path = library_path,
     dbg = dbg
